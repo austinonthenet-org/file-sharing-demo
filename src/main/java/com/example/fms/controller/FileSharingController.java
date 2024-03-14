fc07182023b1bdc1d47324d454ad49640df20afd
Austin Thomas <authomas@liveperson.com>
2024-03-13 19:25:40 -0700

    added inter service call

--- src/main/java/com/example/fms/controller/FileSharingController.java
+++ src/main/java/com/example/fms/controller/FileSharingController.java
@@ -1,30 +1,26 @@
 package com.example.fms.controller;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.cloud.ReadChannel;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.IdTokenCredentials;
+import com.google.auth.oauth2.IdTokenProvider;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
@@ -39,112 +35,100 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("/v1")
 public class FileSharingController {
-    
-    @Value("${NAME:World}")
-    String name;
-    
+
     @Value("${google.storage.bucket.name}")
     String bucketName;
-    
+
     @Value("${google.project.id}")
     String projectId;
-    
+
     @PutMapping("/{brand}/{relative_path}")
-    public String processUpload(HttpServletRequest request,
-            @PathVariable(name = "brand") String brandId, 
+    public String processUpload(HttpServletRequest request, @PathVariable(name = "brand") String brandId,
             @PathVariable(name = "relative_path") String relativePath) {
+
         byte[] body;
+
         try {
+
             body = request.getInputStream().readAllBytes();
-            log.info("fileSize = "+body.length+" bytes.");
+            log.info("fileSize = " + body.length + " bytes.");
             Tika tika = new Tika();
             String mimeType = tika.detect(body);
-            log.info("starting upload to bucket. Mime type = "+mimeType);
+
+            // TODO: call ClamAV for virus scan
+            HttpResponse response = makeGetRequest("https://hello-3fxgfyig7a-uw.a.run.app/", "https://hello-3fxgfyig7a-uw.a.run.app");
+            if (response != null) {
+                log.info("Response body: "+ new String(response.getContent().readAllBytes()));
+            }
+
+            // TODO: call Google Vision API for image profanity
+
+            log.info("starting upload to bucket. Mime type = " + mimeType);
             Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-            BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(bucketName, 
-                    brandId +"/"+ relativePath))
-                    .setContentType(mimeType)
-                    .build();
-            
+            BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(bucketName, brandId + "/" + relativePath))
+                    .setContentType(mimeType).build();
+
             storage.create(blobInfo, body);
             log.info("done upload to bucket.");
         } catch (IOException ex) {
             body = new byte[0];
             log.error("Body parsing exception occurred", ex);
-            //ex.printStackTrace();
+            // ex.printStackTrace();
         }
-      
-        return "Hello " + name + "!";
-      
+
+        return "Success!";
+
     }
     
+    public static HttpResponse makeGetRequest(String serviceUrl, String audience) throws IOException {
+        try {
+            log.info("making inter service call...");
+            GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+            if (!(credentials instanceof IdTokenProvider)) {
+              throw new IllegalArgumentException("Credentials are not an instance of IdTokenProvider.");
+            }
+            IdTokenCredentials tokenCredential =
+                IdTokenCredentials.newBuilder()
+                    .setIdTokenProvider((IdTokenProvider) credentials)
+                    .setTargetAudience(audience)
+                    .build();
+    
+            GenericUrl genericUrl = new GenericUrl(serviceUrl);
+            HttpCredentialsAdapter adapter = new HttpCredentialsAdapter(tokenCredential);
+            HttpTransport transport = new NetHttpTransport();
+            HttpRequest request = transport.createRequestFactory(adapter).buildGetRequest(genericUrl);
+            return request.execute();
+        } catch (Exception e) {
+            log.error("Exception calling other cloud-run service.", e);
+        }
+        return null;
+    }
+
     @GetMapping("/{brand}/{relative_path}")
     public ResponseEntity<byte[]> processDownload(HttpServletRequest request,
-            @PathVariable(name = "brand") String brandId, 
-            @PathVariable(name = "relative_path") String relativePath,
+            @PathVariable(name = "brand") String brandId, @PathVariable(name = "relative_path") String relativePath,
             HttpServletResponse response) {
-        
+
         try {
-            
+
             Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
-            BlobId blobId = BlobId.of(bucketName, 
-                    brandId +"/"+ relativePath);
-            
+            BlobId blobId = BlobId.of(bucketName, brandId + "/" + relativePath);
+
             log.info("starting download.");
             Blob blob = storage.get(blobId);
             if (blob != null) {
                 String mimeType = blob.asBlobInfo().getContentType();
-    
-                //storage.downloadTo(blobId, os, null);
-                //ReadChannel reader = blob.getContent();
+
                 return ResponseEntity.ok().contentType(MediaType.valueOf(mimeType)).body(blob.getContent());
             }
             log.info("done download.");
-            
+
         } catch (Exception ex) {
             log.error("Body parsing exception occurred", ex);
-            //ex.printStackTrace();
         }
-      
+
         return ResponseEntity.notFound().build();
-      
+
     }
-    
-//    @PostMapping(value = "/file-upload-path", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity uploadPath(HttpServletRequest request) {
-//
-//      log.info("Upload temp request arrived ");
-//      Storage storage = StorageOptions.newBuilder().setProjectId("graphite-nectar-415602").build().getService();
-//      // Define resource
-//      BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of("fs-landing-bkt", 
-//              "51574755/14b4bda018bbd15030f498a8d5b1296cf54d33f58c8942b15c99ae444e2fcf77_uuid_3f8dd0ca-d54f-480e-976a-93d9f5f3b341_27-02-2024_20-45-38-574.PNG"))
-//              .build();
-//
-//      // Generate Signed URL
-//      Map<String, String> extensionHeaders = new HashMap<>();
-//      extensionHeaders.put("Content-Type", "image/png");
-//      extensionHeaders.put("x-goog-meta-conversation_id", "368a8b9b-a508-4d60-85a8-b6def69e1eb7");
-//      extensionHeaders.put("x-goog-meta-dialog_id", "368a8b9b-a508-4d60-85a8-b6def69e1eb7");
-//      extensionHeaders.put("x-goog-meta-originator_id", "14b4bda018bbd15030f498a8d5b1296cf54d33f58c8942b15c99ae444e2fcf77");
-//      
-//      URL url =
-//              storage.signUrl(
-//                  blobInfo,
-//                  15,
-//                  TimeUnit.MINUTES,
-//                  Storage.SignUrlOption.httpMethod(HttpMethod.PUT),
-//                  Storage.SignUrlOption.withExtHeaders(extensionHeaders),
-//                  Storage.SignUrlOption.withV4Signature());
-//
-//          System.out.println("Generated PUT signed URL:");
-//          System.out.println(url);
-//          System.out.println("You can use this URL with any user agent, for example:");
-//          System.out.println(
-//              "curl -X PUT -H 'Content-Type: application/octet-stream' --upload-file my-file '"
-//                  + url
-//                  + "'");
-//
-//      return  null;
-//    }
 
 }
